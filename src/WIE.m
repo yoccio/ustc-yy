26901b97239599cdae8540074cb8ddb205689f31
yoccio <yoccio@qq.com>
2016-12-05 14:48:49 +0800

    delete old method

--- src/WIE.m
+++ src/WIE.m
@@ -11,14 +11,6 @@ Hmap = zeros(256,1,'double');
 % calculate 
 pixelMap = Cell(:);
 
-% for s = 0:255
-%     prob(s + 1) = size(find(pixelMap == s),1) / numberOfpixel;
-%     if(prob(s + 1) == 0)
-%         prob(s + 1) = 1;
-%     end
-%     H = H - (s - sAverage)^2 * prob(s + 1) * log(prob(s + 1));
-% end
-
 for pix = 1:length(pixelMap)
     s_temp = Cell(pix);
     prob(s_temp+1) = prob(s_temp +1) + 1 / numberOfpixel;
